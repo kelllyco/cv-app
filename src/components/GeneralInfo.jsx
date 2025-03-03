6c59702d832c03cb1c7b376ac63051c71b88339a
kellycochran <kac1011@icloud.com>
2025-03-03 17:44:37 -0500

    general info css done

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -3,25 +3,25 @@ function GeneralInfo({data, onChange, editMode}) {
         <>
             <h4>general info</h4>
             {editMode ?
-            <>
-                <div>
-                    name
-                    <input type="text" name="name" value={data.name} onChange={onChange}/>
+            <form>
+                <div className="mb-2">
+                    <label for ="name" className="form-label">name</label>
+                    <input class="form-control" type="text" id="name" value={data.name} onChange={onChange}/>
                 </div>
-                <div>
-                    email
-                    <input type="text" name="email" value={data.email} onChange={onChange}/>
+                <div className="mb-2">
+                    <label for="email" className="form-label">email</label>
+                    <input class="form-control" type="text" id="email" value={data.email} onChange={onChange}/>
                 </div>
-                <div>
-                    phone
-                    <input type="text" name="phone" value={data.phone} onChange={onChange} placeholder="Phone"/>
+                <div className="mb-2">
+                    <label for="phone" className="form-label">phone</label>
+                    <input class="form-control" type="text" id="phone" value={data.phone} onChange={onChange} placeholder="Phone"/>
                 </div>
-            </>
+            </form>
             :
             <div>
-                <p>name: {data.name}</p>
-                <p>email: {data.email}</p>
-                <p>phone: {data.phone}</p>
+                <div>{data.name}</div>
+                <div>{data.email}</div>
+                <div>{data.phone}</div>
             </div>
     }
 
